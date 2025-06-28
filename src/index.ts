6e492528baf60a859d540e2211bd3b61282b35a7
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2025-06-28 22:51:25 +0200

    feat: :tada: init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+#! /usr/bin/env bun
+
+import packagejson from "../package.json";
+import { Clipse } from "clipse";
+import {intro, outro} from "@clack/prompts";
+
+const danee = new Clipse("danee", packagejson.description, packagejson.version);
+danee.action(() => {
+    intro("Danee is ready to help you with your tasks.");
+    outro("A new framework is born.");
+}).ready();
