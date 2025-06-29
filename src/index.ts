73ad3fff37f0cd35c63e10d59335625e3bc287d8
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2025-06-29 19:26:24 +0200

    feat: :rocket: configuration of biome

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@
 
 import packagejson from "../package.json";
 import { Clipse } from "clipse";
-import {intro, outro} from "@clack/prompts";
+import { intro, outro } from "@clack/prompts";
 
 const danee = new Clipse("danee", packagejson.description, packagejson.version);
-danee.action(() => {
+danee
+  .action(() => {
     intro("Danee is ready to help you with your tasks.");
     outro("A new framework is born.");
-}).ready();
+  })
+  .ready();
