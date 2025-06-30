3933e4facf56b955213079b669be6ef78cb92cd7
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2025-06-30 19:07:00 +0200

    feat: :sparkles: add colors & logo

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@
 import packagejson from "../package.json";
 import { Clipse } from "clipse";
 import { intro, outro } from "@clack/prompts";
+import pc from "picocolors";
 
 const danee = new Clipse("danee", packagejson.description, packagejson.version);
 danee
   .action(() => {
-    intro("Danee is ready to help you with your tasks.");
+    intro(
+      `${pc.bold(`${pc.bgCyan(pc.whiteBright(" |> "))} ${pc.cyan("Danee")}`)} is ready to help you with your tasks.`,
+    );
     outro("A new framework is born.");
   })
   .ready();
