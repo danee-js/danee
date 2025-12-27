13170cad040c0df0f8cf974b337affcf353e30dc
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2025-12-27 21:24:45 +0100

    refactor: remove try catch in checkLastVersion

--- src/utils/checkLastVersion.ts
+++ src/utils/checkLastVersion.ts
@@ -1,24 +1,20 @@
 import { log } from "@clack/prompts";
 
 export default async function checkVersion(currentVersion: string) {
-  try {
-    const github = await fetch(
-      "https://api.github.com/repos/danee-js/danee/releases/latest",
-      {
-        signal: AbortSignal.timeout(2000), // wait at least 2 seconds before aborting
-      },
-    );
-    if (github.ok) {
-      const data = (await github.json()) as { tag_name: string };
-      if (data.tag_name !== currentVersion) {
-        log.warn(`Danee is out of date! Please update to ${data.tag_name}`);
-      }
-    } else if (github.status === 404) {
-      log.warn("Danee is not available on GitHub");
-    } else {
-      log.error("Failed to check for updates");
+  const github = await fetch(
+    "https://api.github.com/repos/danee-js/danee/releases/latest",
+    {
+      signal: AbortSignal.timeout(2000), // wait at least 2 seconds before aborting
+    },
+  );
+  if (github.ok) {
+    const data = (await github.json()) as { tag_name: string };
+    if (data.tag_name !== currentVersion) {
+      log.warn(`Danee is out of date! Please update to ${data.tag_name}`);
     }
-  } catch (_) {
+  } else if (github.status === 404) {
+    log.warn("Danee is not available on GitHub");
+  } else {
     log.error("Failed to check for updates");
   }
 }
